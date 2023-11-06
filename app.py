8d04f4553d163d30089c905cb4e6247729c301ec
Replit user <>
2023-11-06 10:16:56 +0000

    first web app

---
+++ app.py
@@ -0,0 +1,13 @@
+from os import truncate
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+  return 'Hello, Joly!'
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
